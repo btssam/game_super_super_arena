1f96dff10e75454afe9740e09956f7583959adec
btssam <btsamara@gmail.com>
2026-08-09 05:37:33 -0400

    implemented movement and animated

--- Fighter/Units/f_player_temp.gd
+++ Fighter/Units/f_player_temp.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var horizontal_movement_axis := 0.0
 var vertical_movement_axis := 0.0
-var movement_speed := 100.0
+var movement_speed := 200.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,15 +12,23 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	horizontal_movement_axis = Input.get_axis("Left", "Right")
-	print(horizontal_movement_axis)
+	#print(horizontal_movement_axis)
 	vertical_movement_axis = Input.get_axis("Up", "Down")
-	print(vertical_movement_axis)
+	#print(vertical_movement_axis)
 	%CharacterBody2D.velocity.x = horizontal_movement_axis * movement_speed
+	if %CharacterBody2D.velocity.x != 0.0:
+		%AnimatedSprite2D.play("move")
+		%AnimatedSprite2D.speed_scale = abs(horizontal_movement_axis)
+		%AnimatedSprite2D.flip_h = horizontal_movement_axis < 0
+	else:
+		%AnimatedSprite2D.play("default")
+		%AnimatedSprite2D.speed_scale = 1.0
 	%CharacterBody2D.velocity.y = vertical_movement_axis * movement_speed
 	%CharacterBody2D.move_and_slide()
+	
 
 
-
+#from other script
 #func handle_horizontal_movement() -> void:
 	
 	#if is_climbing:
