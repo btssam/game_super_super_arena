ff749404cb6b35164f89f71ffc131ff4af655088
btssam <btsamara@gmail.com>
2026-08-09 22:16:49 -0400

    added simple jab animation

--- Fighter/Units/f_player_temp.gd
+++ Fighter/Units/f_player_temp.gd
@@ -11,34 +11,33 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	if !isAnimating():
+		handle_movement()
+		handle_attack()
+
+func handle_movement():
 	horizontal_movement_axis = Input.get_axis("Left", "Right")
-	#print(horizontal_movement_axis)
 	vertical_movement_axis = Input.get_axis("Up", "Down")
-	#print(vertical_movement_axis)
 	%CharacterBody2D.velocity.x = horizontal_movement_axis * movement_speed
+	%CharacterBody2D.velocity.y = vertical_movement_axis * movement_speed
 	if %CharacterBody2D.velocity.x != 0.0:
 		%AnimatedSprite2D.play("move")
-		#%AnimatedSprite2D.speed_scale = abs(horizontal_movement_axis)
 		%AnimatedSprite2D.flip_h = horizontal_movement_axis < 0
 	elif %CharacterBody2D.velocity.y != 0.0:
 		%AnimatedSprite2D.play("move")
 	else:
 		%AnimatedSprite2D.play("default")
-		#%AnimatedSprite2D.speed_scale = 1.0
-	%CharacterBody2D.velocity.y = vertical_movement_axis * movement_speed
 	%CharacterBody2D.move_and_slide()
 	
 
+func handle_attack():
+	if Input.is_action_just_pressed("A"):
+		%AnimatedSprite2D.play("jab")
 
-#from other script
-#func handle_horizontal_movement() -> void:
-	
-	#if is_climbing:
-		#%CharacterBody2D.velocity.x = 0
-	#else:
-		#if input_axis:
-			#%CharacterBody2D.velocity.x = input_axis * speed
-		#else: #stopping gradually
-			#%CharacterBody2D.velocity.x = move_toward(%CharacterBody2D.velocity.x, 0, speed)
-	#%CharacterBody2D.move_and_slide()
-	#update_animations(input_axis)
+func isAnimating():
+	if %AnimatedSprite2D.is_playing():
+#	add all uninteruptable animations here
+		if %AnimatedSprite2D.animation == "jab": 
+			return true
+		else:
+			return false
