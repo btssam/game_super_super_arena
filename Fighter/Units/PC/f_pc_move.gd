874d0d6bb40c48b0a409de392029e16b9ac22274
btssam <btsamara@gmail.com>
2026-08-10 23:21:10 -0400

    restructured fighter_pc hierarchy

--- Fighter/Units/PC/f_pc_move.gd
+++ Fighter/Units/PC/f_pc_move.gd
@@ -1,12 +1,14 @@
-extends Node2D
+extends Node
 
 var horizontal_movement_axis := 0.0
 var vertical_movement_axis := 0.0
 var movement_speed := 200.0
 
-# Called when the node enters the scene tree for the first time.
+@onready var character_body_node = get_owner()
+
+
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,20 +17,20 @@ func _process(_delta):
 		handle_movement()
 		handle_attack()
 
+
 func handle_movement():
 	horizontal_movement_axis = Input.get_axis("Left", "Right")
 	vertical_movement_axis = Input.get_axis("Up", "Down")
-	%CharacterBody2D.velocity.x = horizontal_movement_axis * movement_speed
-	%CharacterBody2D.velocity.y = vertical_movement_axis * movement_speed
-	if %CharacterBody2D.velocity.x != 0.0:
+	character_body_node.velocity.x = horizontal_movement_axis * movement_speed
+	character_body_node.velocity.y = vertical_movement_axis * movement_speed
+	if character_body_node.velocity.x != 0.0:
 		%AnimatedSprite2D.play("move")
 		%AnimatedSprite2D.flip_h = horizontal_movement_axis < 0
-	elif %CharacterBody2D.velocity.y != 0.0:
+	elif character_body_node.velocity.y != 0.0:
 		%AnimatedSprite2D.play("move")
 	else:
 		%AnimatedSprite2D.play("default")
-	%CharacterBody2D.move_and_slide()
-	
+	character_body_node.move_and_slide()
 
 func handle_attack():
 	if Input.is_action_just_pressed("A"):
