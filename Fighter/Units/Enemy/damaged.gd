7e99b7f2751b3d5903a15b320105ab5c5ecd0bfc
btssam <btsamara@gmail.com>
2026-08-12 15:55:00 -0400

    added dead animation

--- Fighter/Units/Enemy/damaged.gd
+++ Fighter/Units/Enemy/damaged.gd
@@ -5,7 +5,9 @@ class_name EnemyDamaged
 @export var hp := 3
 
 func _ready():
-	%AnimatedSprite2D.animation_finished.connect(_on_animation_finished)
+	%AnimatedSprite2D.animation_finished.connect(_on_sprite_animation_finished)
+	%AnimationPlayer.animation_finished.connect(_on_player_animation_finished)
+	
 	%ProgressBar.max_value = max_hp
 	%ProgressBar.value = hp
 	
@@ -16,9 +18,16 @@ func hit_by_jab(damage):
 	if hp > 0:
 		%AnimatedSprite2D.play("hurt")
 	else:
-		print('dead')
+		%AnimatedSprite2D.stop()
+		%AnimationPlayer.play("dead")
 	
 
-func _on_animation_finished():
+func _on_sprite_animation_finished():
 	if %AnimatedSprite2D.animation == 'hurt':
 		%AnimatedSprite2D.play("default")
+
+func _on_player_animation_finished(anim_name: StringName):
+	if anim_name == "dead":
+		print('queue_free?')
+	else:
+		print(anim_name + " animationplayer animation has finished.")
