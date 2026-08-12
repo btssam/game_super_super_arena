2c1ba4ca189a6b272afd0c026fa35995fd21d87c
btssam <btsamara@gmail.com>
2026-08-12 02:58:19 -0400

    added enemy hurt animation when hit by jab

--- Fighter/Units/Enemy/damaged.gd
+++ Fighter/Units/Enemy/damaged.gd
@@ -1,5 +1,13 @@
 class_name EnemyDamaged
 extends Node
 
+func _ready():
+	%AnimatedSprite2D.animation_finished.connect(_on_animation_finished)
+
 func hit_by_jab():
-	print('owchy')
+	%AnimatedSprite2D.play("hurt")
+
+func _on_animation_finished():
+	if %AnimatedSprite2D.animation == 'hurt':
+		%AnimatedSprite2D.play("default")
+		print('done')
